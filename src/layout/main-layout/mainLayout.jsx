5e012b734cbdc085374d8f8eb50316f05efcac1f
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-01 21:18:42 +0330

    fix: style layout

--- src/layout/main-layout/mainLayout.jsx
+++ src/layout/main-layout/mainLayout.jsx
@@ -1,11 +1,11 @@
 export default function MainLayout({ children }) {
   return (
     <div className="flex flex-col h-screen">
-      <header className="bg-[#203040] text-white text-center py-2 text-lg">
+      <header className="bg-[#203040] text-white text-center py-4 text-lg">
         React Shopping Cart
       </header>
       <main className="flex-grow">{children}</main>
-      <footer className="bg-[#203040] text-white text-center py-2 text-lg">
+      <footer className="bg-[#203040] text-white text-center py-4 text-lg">
         All right is reserved
       </footer>
     </div>
