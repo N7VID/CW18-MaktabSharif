5ab7c1d946d9c4c4f37384832b0820a2c5b76ed6
Navid Aliakbari <navidaliakbari.206@gmail.com>
2024-08-02 14:25:55 +0330

    fix: filterBar component

--- src/components/productsContainer/productsContainer.jsx
+++ src/components/productsContainer/productsContainer.jsx
@@ -4,28 +4,27 @@ import Product from "../product/product";
 import axios from "axios";
 
 export default function ProductsContainer() {
-  const {data, isLoading, isError,error} = useQuery({
+  const { data, isLoading, isError, error } = useQuery({
     queryKey: ["products"],
-    queryFn: async()=>{
-      const res = await axios.get(`http://localhost:3000/products`)
-      //console.log(res.data);
-      return res.data
-    }
+    queryFn: async () => {
+      const res = await axios.get(`http://localhost:3000/products`);
+      return res.data;
+    },
+  });
 
-  })
-
-  if(isError){
-    return(
-      <div>{error.message}</div>
-    )
+  if (isError) {
+    return <div>{error.message}</div>;
   }
-  
 
   return (
     <div>
       <FilterBar />
       <section className="grid grid-cols-3 gap-4 py-4">
-      {isLoading?<div>LOADING...</div> : data.map((item)=><Product item={item} key={item.id}/>)}
+        {isLoading ? (
+          <div>LOADING...</div>
+        ) : (
+          data.map((item) => <Product item={item} key={item.id} />)
+        )}
       </section>
     </div>
   );
